9fa9155f4cb66cebf1efab36a074080507b05926
bullsean <41093836+bullsean@users.noreply.github.com>
2019-03-29 06:19:39 -0500

    Added login, logout, and auth status changes

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,16 @@
+//listen for auth status changes
+auth.onAuthStateChanged(function(user) {
+    // console.log(user);
+    if(user) {
+        console.log("User logged in: ", user)
+    } else {
+        console.log("User logged out");
+    };
+});
+
+
 var signupForm = $("#signup-form");
-signupForm.on("submit", function (event) {
+signupForm.on("submit", function(event) {
     event.preventDefault();
     //1. get user input
     var password = $("#signup-password")
@@ -9,11 +20,37 @@ signupForm.on("submit", function (event) {
         .val()
         .trim();
     //sign up user
-    auth.createUserWithEmailAndPassword(email, password).then(function(cred) {
-        console.log(cred.user);
+    auth.createUserWithEmailAndPassword(email, password).then(function (cred) {
         const modal = $("#modal-signup");
         M.Modal.getInstance(modal).close();
         signupForm[0].reset();
-
-    })
+    });
 });
+
+//logout
+var logout = $("#logout");
+logout.on("click", function(event) {
+    event.preventDefault();
+    auth.signOut().then(function () {
+    });
+});
+
+//login
+var loginForm = $("#login-form");
+loginForm.on("submit", function(event) {
+    event.preventDefault();
+    //get user info
+    var password = $("#login-password")
+        .val()
+        .trim();
+    var email = $("#login-email")
+        .val()
+        .trim();
+    auth.signInWithEmailAndPassword(email, password).then(function(cred) {
+        //close the login modal and reset the form
+        const modal = $("#modal-login");
+        M.Modal.getInstance(modal).close();
+        loginForm[0].reset();
+    });
+});
+
